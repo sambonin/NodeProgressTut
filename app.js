8fdb110ccb83cf034d3ee2a948efbfd9b6753bb6
Sam Bonin <sambonin777@gmail.com>
2025-10-01 13:20:22 -0500

    3rd day commiting, hard to focus

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+const EventEmitter = require('events')
 
+const customEmitter = new EventEmitter()
 
+customEmitter.on('response', (name, id) => {
+    console.log(`data recieved user ${name},with id: ${id}`)
+})
 
+customEmitter.on('response', () => {
+    console.log(`some other logic here`)
+})
 
-
-
-
-
-
-
-// const _ = require('lodash');
-
-// const items = [1, [2, [3, [4]]]]
-// const newItems = _.flattenDeep(items);
-
-// console.log(newItems);
-// console.log('hello people')
+customEmitter.emit('response', 'john', 34)
